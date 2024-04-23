f49a7cb353a2218dc549d9119d8002c16f2c4d73
Nguyen Huu Trong <52421667+tronglacnhue@users.noreply.github.com>
2024-04-23 16:28:19 +0700

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,101 +1,105 @@
-import cv2 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import os
 
 
-# Load an image from file as function
-def load_image(image_path):
+def read_img(img_path):
     """
-    Load an image from file, using OpenCV
+        Read grayscale image
+        Inputs:
+        img_path: str: image path
+        Returns:
+        img: cv2 image
     """
-    img = cv2.imread(image_path)
-    if img is None:
-        raise Exception("Could not open or find the image.")
-    return img
+    return cv2.imread(img_path, 0)
 
-# Display an image as function
-def display_image(image, title="Image"):
+
+def padding_img(img, filter_size=3):
     """
-    Display an image using matplotlib. Rembember to use plt.show() to display the image
+    The surrogate function for the filter functions.
+    The goal of the function: replicate padding the image such that when applying the kernel with the size of filter_size, the padded image will be the same size as the original image.
+    WARNING: Do not use the exterior functions from available libraries such as OpenCV, scikit-image, etc. Just do from scratch using function from the numpy library or functions in pure Python.
+    Inputs:
+        img: cv2 image: original image
+        filter_size: int: size of square filter
+    Return:
+        padded_img: cv2 image: the padding image
     """
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.title(title)
-    plt.axis('off')
+  # Need to implement here
+
+def mean_filter(img, filter_size=3):
+    """
+    Smoothing image with mean square filter with the size of filter_size. Use replicate padding for the image.
+    WARNING: Do not use the exterior functions from available libraries such as OpenCV, scikit-image, etc. Just do from scratch using function from the numpy library or functions in pure Python.
+    Inputs:
+        img: cv2 image: original image
+        filter_size: int: size of square filter,
+    Return:
+        smoothed_img: cv2 image: the smoothed image with mean filter.
+    """
+  # Need to implement here
+
+def median_filter(img, filter_size=3):
+    """
+        Smoothing image with median square filter with the size of filter_size. Use replicate padding for the image.
+        WARNING: Do not use the exterior functions from available libraries such as OpenCV, scikit-image, etc. Just do from scratch using function from the numpy library or functions in pure Python.
+        Inputs:
+            img: cv2 image: original image
+            filter_size: int: size of square filter
+        Return:
+            smoothed_img: cv2 image: the smoothed image with median filter.
+    """
+  # Need to implement here
+
+
+def psnr(gt_img, smooth_img):
+    """
+        Calculate the PSNR metric
+        Inputs:
+            gt_img: cv2 image: groundtruth image
+            smooth_img: cv2 image: smoothed image
+        Outputs:
+            psnr_score: PSNR score
+    """
+    # Need to implement here
+
+
+
+def show_res(before_img, after_img):
+    """
+        Show the original image and the corresponding smooth image
+        Inputs:
+            before_img: cv2: image before smoothing
+            after_img: cv2: corresponding smoothed image
+        Return:
+            None
+    """
+    plt.figure(figsize=(12, 9))
+    plt.subplot(1, 2, 1)
+    plt.imshow(before_img, cmap='gray')
+    plt.title('Before')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(after_img, cmap='gray')
+    plt.title('After')
     plt.show()
 
 
-# grayscale an image as function
-def grayscale_image(image):
-    """
-    Convert an image to grayscale. Convert the original image to a grayscale image. In a grayscale image, the pixel value of the
-    3 channels will be the same for a particular X, Y coordinate. The equation for the pixel value
-    [1] is given by:
-        p = 0.299R + 0.587G + 0.114B
-    Where the R, G, B are the values for each of the corresponding channels. We will do this by
-    creating an array called img_gray with the same shape as img
-    """
-    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return img_gray
+if __name__ == '__main__':
+    img_noise = "" # <- need to specify the path to the noise image
+    img_gt = "" # <- need to specify the path to the gt image
+    img = read_img(img_noise)
+    filter_size = 3
 
+    # Mean filter
+    mean_smoothed_img = mean_filter(img, filter_size)
+    show_res(img, mean_smoothed_img)
+    print('PSNR score of mean filter: ', psnr(img, mean_smoothed_img))
 
-# Save an image as function
-def save_image(image, output_path):
-    """
-    Save an image to file using OpenCV
-    """
-    cv2.imwrite(output_path, image)
+    # Median filter
+    median_smoothed_img = median_filter(img, filter_size)
+    show_res(img, median_smoothed_img)
+    print('PSNR score of median filter: ', psnr(img, median_smoothed_img))
 
-
-# flip an image as function 
-def flip_image(image):
-    """
-    Flip an image horizontally using OpenCV
-    """
-    return cv2.flip(image, 0)
-
-
-# rotate an image as function
-def rotate_image(image, angle):
-    """
-    Rotate an image using OpenCV. The angle is in degrees
-    """
-    height, width = image.shape[:2]
-    center = (width // 2, height // 2)
-
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    return cv2.warpAffine(image, M, (width, height))
-
-
-if __name__ == "__main__":
-    # Load an image from file
-    img = load_image("images/uet.png")
-
-    # Display the image
-    display_image(img, "Original Image")
-
-    # Convert the image to grayscale
-    img_gray = grayscale_image(img)
-
-    # Display the grayscale image
-    display_image(img_gray, "Grayscale Image")
-
-    # Save the grayscale image
-    save_image(img_gray, "images/lena_gray.jpg")
-
-    # Flip the grayscale image
-    img_gray_flipped = flip_image(img_gray)
-
-    # Display the flipped grayscale image
-    display_image(img_gray_flipped, "Flipped Grayscale Image")
-
-    # Rotate the grayscale image
-    img_gray_rotated = rotate_image(img_gray, 45)
-
-    # Display the rotated grayscale image
-    display_image(img_gray_rotated, "Rotated Grayscale Image")
-
-    # Save the rotated grayscale image
-    save_image(img_gray_rotated, "images/lena_gray_rotated.jpg")
-
-    # Show the images
-    plt.show() 
